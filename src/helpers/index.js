5dd9279de13aca507b07014f90ecf17af8e9f7a4
Igor Dranichnikov <i.dranichnikov@gmail.com>
2019-09-05 12:42:39 +0400

    Add helpers file

---
+++ src/helpers/index.js
@@ -0,0 +1,14 @@
+export const moveElement = (arr, oldIndex, newIndex) => {
+  if (oldIndex > arr.length - 1 || newIndex > arr.length - 1) {
+    console.log("Position exceeds array length");
+    return arr;
+  }
+
+  const _arr = [...arr];
+
+  const element = _arr.splice(oldIndex, 1);
+  const leftArr = _arr.slice(0, newIndex);
+  const rightArr = _arr.slice(newIndex);
+
+  return [...leftArr, ...element, ...rightArr];
+}
