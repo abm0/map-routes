5cceff6f806bfebe510396857db4f214db4a190e
Igor Dranichnikov <i.dranichnikov@gmail.com>
2019-09-05 16:38:10 +0400

    Implement yandex maps interface as react component

--- src/components/MapInterface.js
+++ src/components/MapInterface.js
@@ -1,57 +1,103 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { Map as YMap, Placemark } from 'react-yandex-maps';
-
 class Map extends Component {
-  state = {
+  static propTypes = {
+    pointsById: PropTypes.object.isRequired,
+    ids: PropTypes.array.isRequired,
+  }
+  
+  mapOptions = {
     center: [55.75, 37.57], 
     zoom: 9,
-    points: [],
+    controls: [],
+  }
+
+  createElement() {
+    this.mapEl = document.createElement('div');
+    this.mapEl.id = 'map';
+    document.querySelector('#root .wrapper').appendChild(this.mapEl);
+  }
+
+  removeElement() {
+    document.body.removeChild(this.mapEl);
+  }
+
+  initMap() {
+    const { ymaps } = window;
+    
+    ymaps.ready(() => {
+      this.map = new ymaps.Map('map', this.mapOptions);
+    });
+  }
+
+  createGeoObject(point) {
+    const { ymaps } = window;
+    
+    const coordinates = [
+      parseFloat(point.lng),
+      parseFloat(point.lat),
+    ];
+    
+    return {
+      placemark: new ymaps.Placemark(coordinates),
+      coordinates,
+    };
+  }
+  
+  addGeoObjects(geoobjects) {
+    if (!geoobjects.length) return;
+
+    geoobjects.forEach(geoObject => {
+      this.map.geoObjects.add(geoObject.placemark);
+    });
+  }
+
+  addRoute(geoobjects) {
+    const { ymaps } = window;
+    const coordinates = geoobjects.map(geoobject => geoobject.coordinates);
+    const route = new ymaps.multiRouter.MultiRoute({
+      referencePoints: coordinates,
+    }, {
+      boundsAutoApply: true,
+    });
+
+    this.map.geoObjects.add(route);
+  }
+  
+  componentDidMount() {
+    this.createElement();
+    this.initMap();
+  }
+
+  componentWillUnmount() {
+    this.removeElement();
   }
   
   componentWillReceiveProps(nextProps) {
-    this.setState({
-      points: Object.values(nextProps.pointsById),
-    });
-  }
-  
-  handleMapLoad = () => {}
-
-  renderPlacemarks(points) {
-    if (!points.length) return null;
-
-    const pointsArr = points.map((point, index) => {
-      const coordinates = [
-        parseFloat(point.lng),
-        parseFloat(point.lat),
-      ];
-      
-      return (
-        <Placemark key={index} geometry={coordinates} />
-      );
+    const { ids, pointsById } = nextProps;
+    const orderedPoints = [];
+    
+    ids.forEach(id => {
+      orderedPoints.push(pointsById[id]);
     });
 
-    return pointsArr;
-  }
-  
-  render() {
-    const { points, ...restState } = this.state;
+    const geoobjects = orderedPoints.map(point => (
+      this.createGeoObject(point)
+    ));
 
-    return (
-      <YMap
-        className="map"
-        defaultState={restState}
-        onLoad={this.handleYMapsLoad}
-      >
-        {this.renderPlacemarks(points)}
-      </YMap>
-    );
+    this.map.geoObjects.removeAll();
+    this.addGeoObjects(geoobjects);
+    this.addRoute(geoobjects);
   }
+    
+  render = () => null
 };
 
 const mapStateToProps = (state) => ({
   pointsById: state.points.byId,
+  ids: state.points.ids,
 });
 
 export default connect(mapStateToProps, null)(Map);
