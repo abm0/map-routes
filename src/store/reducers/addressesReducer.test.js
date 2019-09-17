0c816d881a95072ad25c26be55fb2897981e8eb4
Igor Dranichnikov <i.dranichnikov@gmail.com>
2019-09-17 15:44:43 +0400

    Add action creators and reducers tests

--- src/store/reducers/addressesReducer.test.js
+++ src/store/reducers/addressesReducer.test.js
@@ -1,7 +1,63 @@
+import addressesReducer, { initialAddressesState } from './addressesReducer';
+import * as actionCreators from '../actionCreators';
 import * as actionTypes from '../actionTypes';
 
 describe('addresses reducer', () => {
-  it('', () => {
+  const reducerWithInitialState = (action) => addressesReducer(initialAddressesState, action);
+  
+  describe('on ADDRESS_LIST_FETCH action', () => {
+    it('should set isFetching status to true and reset list array', () => {
+      const expectedState = {
+        list: [],
+        isFetching: true,
+      };
+      
+      const addressValue = 'adderessValue';
+      const action = actionCreators.fetchAddressList(addressValue);
+      const actualState = reducerWithInitialState(action);
+      
+      expect(actualState).toEqual(expectedState);
+    });
+  });
 
+
+  describe('on ADDRESS_LIST_FETCH_SUCCESS action', () => {
+    it('should add addresses to address list and set isFetching to false', () => {
+      const addressList = [
+        { name: 'address 1' },
+        { name: 'address 2' },
+        { name: 'address 3' },
+      ];
+      
+      const expectedState = {
+        isFetching: false,
+        list: [...addressList],
+      };
+      
+      const action = {
+        type: actionTypes.ADDRESS_LIST_FETCH_SUCCESS,
+        addressList,
+      };
+  
+      const actualState = reducerWithInitialState(action);
+  
+      expect(actualState).toEqual(expectedState);
+    });
+  });
+  
+  describe('on ADDRESS_LIST_FETCH_FAIL action', () => {
+    it('should set isFetching to false and not update address list', () => {
+      const action = {
+        type: actionTypes.ADDRESS_LIST_FETCH_FAIL,
+      };
+  
+      const expectedState = {
+        isFetching: false,
+        list: [],
+      };
+  
+      const actualState = reducerWithInitialState(action);
+      expect(actualState).toEqual(expectedState);
+    });
   });
 });
--- src/store/reducers/pointsReducer.test.js
+++ src/store/reducers/pointsReducer.test.js
@@ -1,7 +1,140 @@
-import * as actionTypes from '../actionTypes';
+import pointsReducer, { initialPointsState } from './pointsReducer';
+import * as actionCreators from '../actionCreators';
 
 describe('points reducer', () => {
-  it('', () => {
+  const reducerWithInitialState = (action) => pointsReducer(initialPointsState, action);
+  
+  describe('on POINT_ADD action', () => {
+    const newId = new Date().getUTCMilliseconds();
+    const newAddress = {
+      lat: 'lat',
+      lng: 'lng',
+    };
 
+    it('should add point to byId object', () => {
+      const expectedState = {
+        byId: {
+          [newId]: {
+            id: newId,
+            ...newAddress,
+          }
+        },
+        ids: [newId],
+      };
+      
+      const action = actionCreators.addPoint(newAddress, newId);
+      const actualState = reducerWithInitialState(action);
+      
+      expect(actualState).toEqual(expectedState);
+    });
+
+    it('should not add point if point with exact same coordinates is present', () => {
+      const initialState = {
+        byId: {
+          [newId]: {
+            id: newId,
+            lat: 'lat',
+            lng: 'lng',
+          }
+        },
+        ids: [newId],
+      };
+
+      const expectedState = { ...initialState };
+
+      const action = actionCreators.addPoint(newAddress, newId);
+      const actualState = pointsReducer(initialState, action);
+
+      expect(actualState).toEqual(expectedState);
+    });
+  });
+
+  describe('with non-empty state', () => {
+    const firstId = 123;
+    const secondId = 456;
+    const thirdId = 789;
+
+    const initialState = {
+      byId: {
+        [firstId]: {
+          id: firstId,
+          lat: 'lat1',
+          lng: 'lng1',
+        },
+        [secondId]: {
+          id: secondId,
+          lat: 'lat2',
+          lng: 'lng2',
+        },
+        [thirdId]: {
+          id: thirdId,
+          lat: 'lat3',
+          lng: 'lng3',
+        },
+      },
+      ids: [firstId, secondId, thirdId],
+    };
+
+    describe('on POINT_REMOVE action', () => {
+      it('should remove point', () => {  
+        const expectedState = {
+          byId: {
+            [secondId]: {
+              ...initialState.byId[secondId],
+            },
+            [thirdId]: {
+              ...initialState.byId[thirdId],
+            },
+          },
+          ids: [secondId, thirdId],
+        };
+  
+        const action = actionCreators.removePoint(firstId);
+        const actualState = pointsReducer(initialState, action);
+  
+        expect(actualState).toEqual(expectedState);
+      });
+    });
+  
+    describe('on POINT_OREDER_CHANGE action', () => {
+      it('should change order of elements in ids array', () => {
+        const oldIndex = 1;
+        const newIndex = 2;
+
+        const expectedState = {
+          byId: {
+            ...initialState.byId,
+          },
+          ids: [firstId, thirdId, secondId],
+        };
+        
+        const action = actionCreators.changePointOrder({ oldIndex, newIndex });
+        const actualState = pointsReducer(initialState, action);
+  
+        expect(actualState).toEqual(expectedState);
+      });
+    });
+  
+    describe('on POINT_POSITION_CHANGE action', () => {
+      it('should change the coordinates of existing point', () => {
+        const newCoordinates = ['lng4', 'lat4'];
+        const expectedState = {
+          ...initialState,
+          byId: {
+            ...initialState.byId,
+            [secondId]: {
+              ...initialState.byId[secondId],
+              lng: 'lng4',
+              lat: 'lat4',
+            }
+          },
+        };
+
+        const action = actionCreators.updatePointPosition(newCoordinates, secondId);
+        const actualState = pointsReducer(initialState, action);
+
+        expect(actualState).toEqual(expectedState);
+      });
+    });
   });
 });
