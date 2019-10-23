28c8fe5ad46c136678db999e62fac8e72262e9f4
Igor Dranichnikov <i.dranichnikov@gmail.com>
2019-10-23 16:58:21 +0400

    Updates

---
+++ precache-manifest.b4b61fc8ab4b8b176ae0d04b75de9651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4388d025fb3b1754ae66303d3549c124",
+    "url": "/map-routes/static/media/spinner.4388d025.svg"
+  },
+  {
+    "revision": "410c26b966245f945a62a61d89ffe8a1",
+    "url": "/map-routes/static/media/icofont.410c26b9.woff2"
+  },
+  {
+    "revision": "16909cc913e82029eb884ad15fdc0669",
+    "url": "/map-routes/static/media/icofont.16909cc9.woff"
+  },
+  {
+    "revision": "045d35948ae80f3e28f8",
+    "url": "/map-routes/static/js/runtime~main.d9ab0a79.js"
+  },
+  {
+    "revision": "5c22258cd7a2388cd320",
+    "url": "/map-routes/static/js/main.09457a1c.chunk.js"
+  },
+  {
+    "revision": "23f9096cfc66e47c01bf",
+    "url": "/map-routes/static/js/2.2b979dfa.chunk.js"
+  },
+  {
+    "revision": "5c22258cd7a2388cd320",
+    "url": "/map-routes/static/css/main.fdc98e24.chunk.css"
+  },
+  {
+    "revision": "511f2e91249cf86c14dd42acd08020f3",
+    "url": "/map-routes/index.html"
+  }
+];
